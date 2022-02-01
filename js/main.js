53f864ea33498a8421cb8dac90ce9109a3ff66b8
Varfolomei <varfolomeisafronov@gmail.com>
2022-02-02 01:29:43 +0200

    statistics

--- js/main.js
+++ js/main.js
@@ -13,3 +13,29 @@ function openDev(evt, devName) {
 }
 
 document.getElementById("defaultOpen").click();
+
+$(document).ready(function () {
+  var show = true;
+  var countbox = ".statistics";
+  $(window).on("scroll load resize", function () {
+    if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+    var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+    var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+    var w_height = $(window).height(); // Высота окна браузера
+    var d_height = $(document).height(); // Высота всего документа
+    var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+    if (
+      w_top + 500 >= e_top ||
+      w_height + w_top == d_height ||
+      e_height + e_top < w_height
+    ) {
+      $(".statistics__number").css("opacity", "1");
+      $(".statistics__number").spincrement({
+        thousandSeparator: "",
+        duration: 2000,
+      });
+
+      show = false;
+    }
+  });
+});
